50d7df6eea12366ae03ee8a768af805d96c6f109
penrodlol <christian@penrod.contact>
2025-03-02 17:32:07 -0500

    ✨ Image window

---
+++ src/components/image-window/index.ts
@@ -0,0 +1,5 @@
+export { default as ContentUpload } from './components/content-upload.astro';
+export { default as Content } from './components/content.astro';
+export { default as Header } from './components/header.astro';
+export { default as Icon } from './components/icon.astro';
+export { default as Root } from './components/root.astro';
