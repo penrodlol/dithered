08c2c0e14a68edf6c257705d8153994718cc3666
penrodlol <christian@penrod.contact>
2025-03-05 22:23:32 -0500

    ✨ Select

--- alpine.config.ts
+++ alpine.config.ts
@@ -1,4 +1,5 @@
+import anchor from '@alpinejs/anchor';
 import focus from '@alpinejs/focus';
 import type { Alpine } from 'alpinejs';
 
-export default (Alpine: Alpine) => Alpine.plugin([focus]);
+export default (Alpine: Alpine) => Alpine.plugin([focus, anchor]);
---
+++ src/components/select/index.ts
@@ -0,0 +1,4 @@
+export { default as Option } from './components/option.astro';
+export { default as Options } from './components/options.astro';
+export { default as Root } from './components/root.astro';
+export { default as Trigger } from './components/trigger.astro';
