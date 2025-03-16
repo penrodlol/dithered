f0e34dc4b209d8cf06c7047684b867141ff06bc2
penrodlol <christian@penrod.contact>
2025-03-15 20:56:43 -0400

    💄 tweaking some descriptions

--- src/components/radio-group/index.ts
+++ src/components/radio-group/index.ts
@@ -1,4 +1,5 @@
+export { default as Description } from './components/description.astro';
 export { default as Item } from './components/item.astro';
 export { default as Items } from './components/items.astro';
-export { default as Legend } from './components/legend.astro';
+export { default as Label } from './components/label.astro';
 export { default as Root } from './components/root.astro';
